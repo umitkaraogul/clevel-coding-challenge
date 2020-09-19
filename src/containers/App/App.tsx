3dfb395c993b4157458bdebddceddaf8ab05530d
Umit <ukaraogul@gmail.com>
2020-09-19 17:26:52 +0300

    React router added and Navigation is developed

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,19 +1,25 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navigation from "../Navigation";
 import Transaction from "../Transaction";
 import Dashboard from "../Dashboard";
 import Setting from "../Setting";
 import Account from "../Account";
 
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <Transaction />
-      <Dashboard />
-      <Setting />
-      <Account />
+      <BrowserRouter>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact={true} component={Dashboard} />
+          <Route path="/transaction" component={Transaction} />
+          <Route path="/accounts" component={Account} />
+          <Route path="/settings" component={Setting} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/containers/Navigation/index.tsx
+++ src/containers/Navigation/index.tsx
@@ -1,3 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default () => <div className="Navigation">Navigation</div>;
+export default () => (
+  <div className="Navigation">
+    <ul>
+      <li>
+        <Link to="/">Dashboard</Link>
+      </li>
+      <li>
+        <Link to="/transaction">Transaction</Link>
+      </li>
+      <li>
+        <Link to="/accounts">Accounts</Link>
+      </li>
+      <li>
+        <Link to="/settings">Settings</Link>
+      </li>
+    </ul>
+  </div>
+);
